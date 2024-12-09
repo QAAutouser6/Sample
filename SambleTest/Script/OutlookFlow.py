f98eefaa461adb66e6cef7a9efbf462ede44b673
NA\skalimuthu_cont <skalimuthu_cont@mwe.com>
2024-12-09 08:39:15 -0600

    Test Git commit

---
+++ SambleTest/Script/OutlookFlow.py
@@ -0,0 +1,9 @@
+﻿import outlook
+
+def lanuchAndCloseOutlook():
+  aqTestCase.Begin("Launch Outlook")
+  outlook.lanuchOutLook()
+  aqTestCase.End()
+  aqTestCase.Begin("Launch Outlook")
+  outlook.closeOutlook()
+  aqTestCase.End()
---
+++ SambleTest/Script/outlook.py
@@ -0,0 +1,14 @@
+﻿def lanuchOutLook():
+  
+  p = TestedApps.OUTLOOK.Run()
+  Delay(10000)
+  wndOutlook = NameMapping.Sys.OUTLOOK.wndrctrl_renwnd32
+  wndOutlook.Maximize()
+  
+def closeOutlook():
+  wndOutlook = NameMapping.Sys.OUTLOOK.wndrctrl_renwnd32
+  wndOutlook.Close()
+  Delay(5000)
+  
+  
+  
